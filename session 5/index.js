f55e9c46460dd78ac8d9836d5a4694c53c842a51
3aB0z <145503861+3aB0z@users.noreply.github.com>
2024-02-12 22:11:37 +0100

    session 5

---
+++ session 5/index.js
@@ -0,0 +1,31 @@
+// console.log(document.querySelector(".nom").value);
+// document.querySelector(".nom").value = "Ahmed";
+
+// console.log(document.querySelector(".age").value);
+
+// let a = Number(document.querySelector(".a").value);
+// let b = Number(document.querySelector(".b").value);
+// let somme = a + b;
+// document.querySelector(".somme").value = somme;
+
+// if (somme >= 20) {
+//     document.querySelector(".somme").style.backgroundColor = "red";
+// } else {
+//     document.querySelector(".somme").style.backgroundColor = "blue";
+// }
+
+// let calculerF = (e) => {
+//     e.preventDefault();
+//     let n1 = Number(document.querySelector(".a").value);
+//     let n2 = Number(document.querySelector(".b").value);
+//     document.querySelector(".somme").textContent = n1 + n2;
+// };
+// document.querySelector(".btn-calculer").addEventListener("click", calculerF);
+
+let userNumber = Number(document.querySelector(".userNumber"));
+let result;
+
+function answer() {
+    userNumber.addEventListener
+}
+
