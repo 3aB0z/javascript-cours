ebf275f748922fc33757d341e591fdb0cc8a8e6e
3aB0z <145503861+3aB0z@users.noreply.github.com>
2024-02-13 12:09:59 +0100

    play again

--- session 5/index.js
+++ session 5/index.js
@@ -28,20 +28,26 @@
 let storage = 20;
 let minResult = 5;
 let maxResult = 18;
+document.querySelector(".points").textContent = "Avaiable points :" + storage;
 let answer = (e) => {
     let userNumber = Number(document.querySelector(".userNumber").value);
     e.preventDefault();
     storage -= 1;
     if (storage > 0) {
+        document.querySelector(".points").textContent = "Avaiable points :" + storage;
         if (userNumber <= maxResult && userNumber >= minResult) {
             document.querySelector(".result").textContent = "You Win!";
+            document.querySelector(".submit").textContent = "Play again";
+            storage = 20;
         } else {
             document.querySelector(".result").textContent = "Try something else";
         }
     } else {
+        document.querySelector(".points").textContent = "Avaiable points :" + storage;
         document.querySelector(".result").textContent = "Your points out of stock!";
         document.querySelector(".submit").textContent = "Play again";
-        document.querySelector(".submit").addEventListener("click", () => {window.location.reload();});
+        storage = 20;
+        document.querySelector(".submit").addEventListener("click", answer);
     }
 };
 document.querySelector(".submit").addEventListener("click", answer);
