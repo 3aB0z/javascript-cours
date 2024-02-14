99f21c738c5840c120f127e1e0ff1e3ea1de8547
3aB0z <145503861+3aB0z@users.noreply.github.com>
2024-02-14 20:41:31 +0100

    play again fixed

--- session 5/index.js
+++ session 5/index.js
@@ -32,21 +32,31 @@ document.querySelector(".points").textContent = "Avaiable points :" + storage;
 let answer = (e) => {
     let userNumber = Number(document.querySelector(".userNumber").value);
     e.preventDefault();
-    storage -= 1;
-    if (storage > 0) {
+    if (storage === 21) {
+        document.querySelector(".result").textContent = "Enter a number";
+        document.querySelector(".result").style.color = "black";
+        document.querySelector(".submit").textContent = "Submit";
+        storage = 20;
+        document.querySelector(".points").textContent = "Avaiable points :" + storage;
+        document.querySelector(".userNumber").value = "";
+    } else if (storage > 0 && storage < 21) {
+        storage -= 1;
         document.querySelector(".points").textContent = "Avaiable points :" + storage;
         if (userNumber <= maxResult && userNumber >= minResult) {
             document.querySelector(".result").textContent = "You Win!";
+            document.querySelector(".result").style.color = "green";
             document.querySelector(".submit").textContent = "Play again";
-            storage = 20;
+            storage = 21;
         } else {
             document.querySelector(".result").textContent = "Try something else";
+            document.querySelector(".result").style.color = "red";
         }
-    } else {
+    }
+    if (storage === 0) {
         document.querySelector(".points").textContent = "Avaiable points :" + storage;
         document.querySelector(".result").textContent = "Your points out of stock!";
         document.querySelector(".submit").textContent = "Play again";
-        storage = 20;
+        storage = 21;
         document.querySelector(".submit").addEventListener("click", answer);
     }
 };
